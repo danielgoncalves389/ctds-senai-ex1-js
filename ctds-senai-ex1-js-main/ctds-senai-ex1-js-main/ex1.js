9da64b095dda9ee7f741892e2951ac42c4bfe14e
Daniel Gonçalves <dn648979@gmail.com>
2025-05-16 19:18:00 -0300

    Add files via upload

---
+++ ctds-senai-ex1-js-main/ctds-senai-ex1-js-main/ex1.js
@@ -0,0 +1,23 @@
+/*
+Instruções:
+Crie um HTML básico e inclua o link externo para o arquivo script.js
+
+No JavaScript:
+Crie duas variáveis com números e exiba a soma delas no console
+
+Mostre no console.log() uma mensagem assim:
+console.log("A soma é:", soma);
+
+Use:
+let
+console.log()
+
+*/
+
+let num1, num2;
+
+num1= 35;
+num2= 56;
+
+let soma = num1 + num2;
+console.log("A soma é: ", soma);
